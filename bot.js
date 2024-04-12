dfde9761d1e038bfb4b687833283ddb0b203c142
pedromaia999 <158615952+pedromaia999@users.noreply.github.com>
2024-04-12 14:46:09 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,39 @@
+// Imports
+const SteamUser = require("steam-user");
+
+// Config
+const config = require("./config.json");
+
+for (const account in config.accounts) {
+    const username = config.accounts[account].details.username;
+    const password = config.accounts[account].details.password;
+
+    const title = config.accounts[account].settings.title;
+    const games = config.accounts[account].settings.games;
+
+    // Authorization
+    const client = new SteamUser();
+
+    client.logOn({
+        accountName: username,
+        password: password,
+    });
+
+    // Main
+    client.on("loggedOn", async () => {
+        await Array.prototype.push.apply(
+            title, 
+            games
+        );
+
+        await client.setPersona(
+            SteamUser.EPersonaState.Online
+        ); 
+
+        await client.gamesPlayed(
+            title
+        ); 
+        
+        console.log(`Logged succesfully as ${username}.`);
+    });
+}
